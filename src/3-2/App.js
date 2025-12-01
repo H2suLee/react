b015f4ec158000bccf0edac87d34c75f721f7cd6
이희수 <h2su0712@naver.com>
2025-12-01 14:22:11 +0900

    Props

---
+++ src/3-2/App.js
@@ -0,0 +1,15 @@
+import Hello from "./components/Hello";
+import World from "./components/World";
+import Heading from "./components/Heading";
+
+// Heading 이라는 컴포넌트를 만들고 Hello, World 컴포넌트를 통합하기
+export default function App() {
+  return (
+    <div>
+      <Heading type="h2">hello</Heading>
+
+      <Hello />
+      <World />
+    </div>
+  );
+}
