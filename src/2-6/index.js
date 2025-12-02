abf1753f532c7b309c6f8838b3fb5d3f46ecf420
H2suLee <h2su0712@naver.com>
2025-12-02 14:23:05 +0900

    구구단

--- src/2-6/index.js
+++ src/2-6/index.js
@@ -1,5 +1,29 @@
 import ReactDOM from "react-dom";
 
-const element = <h1>Hello, world!</h1>;
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const element = (
+  <div style={{ display: "flex" }}>
+    {num
+      .filter((i) => i !== 1 && i !== 5)
+      .map((i) => {
+        return (
+          <div
+            key={i}
+            style={{
+              marginRight: "20px",
+              color: i % 2 === 1 ? "blue" : "black",
+            }}
+          >
+            {num.map((j) => (
+              <div key={j}>
+                {i} x {j} = {i * j}
+              </div>
+            ))}
+          </div>
+        );
+      })}
+  </div>
+);
 
 ReactDOM.render(element, document.getElementById("root"));
